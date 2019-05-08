7ac9e093ed5c8e66f152b6fd1aa57b832c761c98
Casey Bee <caseye.baker@gmail.com>
2019-05-08 15:23:34 -0700

    dictionaries are not lists; I think my logic is on the right track but syntax and methods need work

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -3,11 +3,24 @@
 import math
 
 def recipe_batches(recipe, ingredients):
-    # batches = 0
-    # if ingredients length < recipe length (amount of keys)
-        # return batches
-    # if ingredients keys don't match recipe keys
-        # return batches
+    batches = 0
+    if len(ingredients) < len(recipe):
+            return batches
+    if recipe.keys() != ingredients.keys(): #assumption: ingredient and recipe keys are sorted
+        return batches
+    i = 0
+    k = 0
+    for i in range(0, len(recipe)):
+        if ingredients[k] < recipe[k]:
+            batches = 0
+            return batches
+        else:
+            result = (ingredients[k] / recipe[k])
+            if result < batches:
+                batches = result
+                k +=1
+    return batches
+
     # compare values for each key
         # if current ingredient val < current recipe val
             # batches = 0
@@ -18,8 +31,20 @@ def recipe_batches(recipe, ingredients):
             # batches = result
             # continue
     # return batches
-  pass 
+pass 
 
+recipe1 = { 'milk': 100, 'butter': 50, 'flour': 5 }
+recipe2 = { 'milk': 100, 'butter': 50, 'flour': 5 }
+ingredients1 = { 'milk': 132, 'butter': 48 }
+ingredients2 = { 'milk': 132, 'eggs': 48, 'flour': 51 }
+
+recipe3 = { 'milk': 100, 'butter': 50, 'flour': 5 }
+ingredients3 = { 'milk': 200, 'butter': 60, 'flour': 51 }
+recipe4 = { 'milk': 100, 'butter': 50, 'flour': 5 }
+ingredients4 = { 'milk': 100, 'butter': 50, 'flour': 0 }
+
+print(recipe_batches(recipe1, ingredients1))
+print(recipe_batches(recipe2, ingredients2))
 
 if __name__ == '__main__':
   # Change the entries of these dictionaries to test 
